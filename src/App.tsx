7f92d3d09f9c6fb0fe1fad979718f5593ec9e27f
Renan Lima <renanlimabl@gmail.com>
2021-05-09 00:39:36 -0300

    add modal estilizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransctionModalOpen, setIsNewTransctionModalOpen] = useState(false);
+
+  function handleOpenNewTransctionModal() {
+    setIsNewTransctionModalOpen(true)
+  }
+
+  function handleCloseNewtransactionModal() {
+    setIsNewTransctionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransctionModal} />
       <Dashboard />
+      <NewTransactionModal isOpen={isNewTransctionModalOpen} onRequestClose={handleCloseNewtransactionModal} />
       <GlobalStyle />
     </>
   );
